b5dc04c21f325d25261655fd0b7bde879910f625
Stas Cherny <stasch@gmail.com>
2015-08-12 19:34:51 +0300

    Add unit test project

---
+++ ChessLibraryUnitTest/PawnMove.cs
@@ -0,0 +1,31 @@
+﻿using ChessLibrary;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessLibrary.Tests
+{
+    [TestClass()]
+    public class PawnMove
+    {
+        [TestMethod()]
+        public void MoveValidationTest_Success()
+        {            
+            Pawn white_pawn = new Pawn(SetColor.White);
+            Cell origCell = new Cell(1,1);            
+
+            Cell destCell = new Cell(1,3);
+            white_pawn.MoveValidation(origCell, destCell);
+
+            destCell.Y = 2;
+            white_pawn.MoveValidation(origCell, destCell);
+
+            Pawn black_pawn = new Pawn(SetColor.Black);
+            origCell.Y = 7;
+            destCell.Y = 5;
+            black_pawn.MoveValidation(origCell, destCell);
+
+            destCell.Y = 6;
+            black_pawn.MoveValidation(origCell, destCell);
+        }
+    }
+}
